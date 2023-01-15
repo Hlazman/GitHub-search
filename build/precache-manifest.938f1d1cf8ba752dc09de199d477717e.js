007ee86445463a4dcd96aaa552b979a62338d3c8
Hlazman <glazman.b@gmail.com>
2023-01-15 15:47:30 +0200

    Configure React app for deployment to GitHub Pages

---
+++ build/precache-manifest.938f1d1cf8ba752dc09de199d477717e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f23727b14a624b08407b2bfbd8a1be",
+    "url": "/react_gh-search/index.html"
+  },
+  {
+    "revision": "b9f890e2eb8edaf5bb93",
+    "url": "/react_gh-search/static/css/main.c8a395c5.chunk.css"
+  },
+  {
+    "revision": "bcf393503cc39700d3b5",
+    "url": "/react_gh-search/static/js/2.2c67e351.chunk.js"
+  },
+  {
+    "revision": "b9f890e2eb8edaf5bb93",
+    "url": "/react_gh-search/static/js/main.3ab2c271.chunk.js"
+  },
+  {
+    "revision": "32f7ed65029cf195adea",
+    "url": "/react_gh-search/static/js/runtime~main.03ae722a.js"
+  }
+]);
