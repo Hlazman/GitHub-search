a797f70629d745371acdf22d764923197a0e6ddb
Hlazman <glazman.b@gmail.com>
2023-01-15 16:11:47 +0200

    Updates

--- precache-manifest.4154d7bc65250a6a71b8df6e911fb974.js
+++ precache-manifest.4154d7bc65250a6a71b8df6e911fb974.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "14f23727b14a624b08407b2bfbd8a1be",
+    "revision": "bc110e384cb2a66f7cee755396ae9f98",
     "url": "/react_gh-search/index.html"
   },
   {
-    "revision": "b9f890e2eb8edaf5bb93",
+    "revision": "b70b1b675a20d1f28697",
     "url": "/react_gh-search/static/css/main.c8a395c5.chunk.css"
   },
   {
-    "revision": "bcf393503cc39700d3b5",
-    "url": "/react_gh-search/static/js/2.2c67e351.chunk.js"
+    "revision": "21bfcea68644fa333dba",
+    "url": "/react_gh-search/static/js/2.49ea6549.chunk.js"
   },
   {
-    "revision": "b9f890e2eb8edaf5bb93",
-    "url": "/react_gh-search/static/js/main.3ab2c271.chunk.js"
+    "revision": "b70b1b675a20d1f28697",
+    "url": "/react_gh-search/static/js/main.686a6b64.chunk.js"
   },
   {
     "revision": "32f7ed65029cf195adea",
